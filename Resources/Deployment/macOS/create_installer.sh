ded3844c8032ba138194a11b01071a39c389f5e0
Christian Ahrens <christianahrens@me.com>
2020-12-30 11:06:54 +0100

    Refined create_installer.sh to generate notarized dmg installer.

--- Resources/Deployment/macOS/create_installer.sh
+++ Resources/Deployment/macOS/create_installer.sh
@@ -1,5 +1,13 @@
-# change directory to project root
+# we are in Resources/Deployment/macOS/ -> change directory to project root
 cd ../../../
+
+# set external variables
+CodeSignCertName=$1 # e.g. "Developer ID Application: SomeIdentifier"
+NotarizationUser=$2 # appleid to use for notarization
+AppBundleId=$3 # app bundle id like "com.SomeIdentifier.SoundscapeBridgeApp"
+
+echo "Using certificate $CodeSignCertName for codesigning."
+echo "Using AppleId $NotarizationUser and bundle name $AppBundleId for notarization."
 
 # set convenience variables
 JUCEDir=/Applications/JUCE
@@ -20,3 +28,13 @@ xcodebuild -project "$XCodeProjectPath" -configuration Release -jobs 8
 test -f "$DmgTargetPath" && rm "$DmgTargetPath"
 ln -s /Applications "$AppBundlePath"/Applications
 "$CreateDmgPath" --window-size 410 240 "$DmgTargetPath" "$AppBundlePath"
+
+# codesign the disk image
+codesign --force --sign "$CodesignCertName" "$DmgTargetPath"
+
+# trigger notarization
+# (assumes that a keychain item "SCB_DMG_Notarization_PW" exists, created e.g. with "xcrun altool --store-password-in-keychain-item "SCB_DMGNotarization_PW" -u "abc" -p "def"")
+xcrun altool --notarize-app --primary-bundle-id "$AppBundleId" --username "$NotarizationUser" --password "@keychain:SCB_DMGNotarization_PW" --file "$DmgTargetPath"
+
+# notarization result can be queried as follows
+# xcrun altool --notarization-info 2EFE2717-52EF-43A5-96DC-0797E4CA1041 -u "$NotarizationUser"

e1d172c1cce9fcf0bb3beda854163400dda93e89
Christian Ahrens <christianahrens@me.com>
2021-01-04 15:59:46 +0100

    Updated copyright to 2021 and partly fixed licensing disclaimers.

--- Source/PagedUI/PageComponents/StatisticsPageComponent.h
+++ Source/PagedUI/PageComponents/StatisticsPageComponent.h
@@ -1,12 +1,20 @@
-/*
-  ==============================================================================
-
-    StatisticsPageComponent.h
-    Created: 8 Nov 2020 10:19:43am
-    Author:  Christian Ahrens
-
-  ==============================================================================
-*/
+/* Copyright (c) 2020-2021, Christian Ahrens
+ *
+ * This file is part of SoundscapeBridgeApp <https://github.com/ChristianAhrens/SoundscapeBridgeApp>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #pragma once
 

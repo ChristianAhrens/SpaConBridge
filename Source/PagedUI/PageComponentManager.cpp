be08e222fe919727bb3c533cb5fee2a6ff56761b
Christian Ahrens <christianahrens@me.com>
2021-05-21 10:53:51 +0200

    Fixed initialization of PageComponentManager to provide some usable defaults (in case no config xml is present and is created on startup)

--- Source/PagedUI/PageComponentManager.cpp
+++ Source/PagedUI/PageComponentManager.cpp
@@ -69,6 +69,13 @@ PageComponentManager::PageComponentManager()
 
 	// Default overview window properties.
 	m_pageContainer = nullptr;
+
+	// Initialization of members to a usable default makes sense 
+	// when no config xml is available on app start and therefor 
+	// init values of this instance are going to be used for default config.
+	m_activePage = UPI_SoundObjects;
+	for (int id = UPI_InvalidMin + 1; id < UPI_InvalidMax; id++) m_enabledPages.push_back(static_cast<UIPageId>(id));
+	m_lookAndFeelType = DbLookAndFeelBase::LAFT_Dark;
 }
 
 /**

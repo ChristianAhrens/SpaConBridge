c4401e4545f63a7cd601e669305f28c11f170bc8
Christian Ahrens <christianahrens@me.com>
2023-11-14 09:26:44 +0100

    Fixed AppVeyor CI by adding legacy macOS projucer exporter and accompanying build script

---
+++ Resources/Deployment/macOS/build_project_non-Xcode15_legacyCI.sh
@@ -0,0 +1,18 @@
+# we are in Resources/Deployment/macOS/ -> change directory to project root
+cd ../../../
+
+# set convenience variables
+JUCEDir=submodules/JUCE
+ProjucerPath="$JUCEDir"/extras/Projucer/Builds/MacOSX
+ProjucerBinPath="$ProjucerPath"/build/Release/Projucer.app/Contents/MacOS/Projucer
+JucerProjectPath=SpaConBridge.jucer
+XCodeProjectPath=Builds/MacOSX_non-Xcode15_legacyCI/SpaConBridge.xcodeproj
+
+# build projucer
+xcodebuild -project "$ProjucerPath"/Projucer.xcodeproj -configuration Release -jobs 8
+
+# export projucer project
+"$ProjucerBinPath" --resave "$JucerProjectPath"
+
+# start building the project
+xcodebuild -project "$XCodeProjectPath" -configuration Release -jobs 8

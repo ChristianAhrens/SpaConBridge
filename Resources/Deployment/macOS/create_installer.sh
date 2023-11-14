d877418988ebf6470c785805b9153ba4d52b5442
Christian Ahrens <christianahrens@me.com>
2023-11-14 19:10:24 +0100

    Updated create_installer.sh to use new xcrun notarytool for dmg notarization

--- Resources/Deployment/macOS/create_installer.sh
+++ Resources/Deployment/macOS/create_installer.sh
@@ -3,11 +3,8 @@ cd ../../../
 
 # set external variables
 CodeSignCertName=$1 # e.g. "Developer ID Application: SomeIdentifier"
-NotarizationUser=$2 # appleid to use for notarization
-AppBundleId=$3 # app bundle id like "com.SomeIdentifier.SpaConBridge"
 
 echo "Using certificate $CodeSignCertName for codesigning."
-echo "Using AppleId $NotarizationUser and bundle name $AppBundleId for notarization."
 
 # set convenience variables
 AppBundlePath=Builds/MacOSX/build/Release
@@ -24,8 +21,5 @@ cd ../../../
 codesign --force --sign "$CodesignCertName" "$DmgTargetPath"
 
 # trigger notarization
-# (assumes that a keychain item "SCB_DMG_Notarization_PW" exists, created e.g. with "xcrun altool --store-password-in-keychain-item "SCB_DMGNotarization_PW" -u "abc" -p "def"")
-xcrun altool --notarize-app --primary-bundle-id "$AppBundleId" --username "$NotarizationUser" --password "@keychain:SCB_DMGNotarization_PW" --file "$DmgTargetPath"
-
-# notarization result can be queried as follows
-# xcrun altool --notarization-info 2EFE2717-52EF-43A5-96DC-0797E4CA1041 -u "$NotarizationUser"
+# (assumes that credentials are stored to keychain, e.g. with xcrun notarytool store-credentials APP_NOTARIZATION_CREDENTIALS_PROFILE --apple-id APPLE_ID --team-id TEAM_ID)
+xcrun notarytool submit --keychain-profile "APP_NOTARIZATION_CREDENTIALS_PROFILE" --wait "$DmgTargetPath"
